6af81fce833fc9ae44e190706037a507659b0ffe
edvinf <edvin.fuglebakk@gmail.com>
2020-11-08 11:23:03 +0100

    Added spatial resources for fdir locations

--- inst/dataPrepScripts/prepSpatialResources.R
+++ inst/dataPrepScripts/prepSpatialResources.R
@@ -1,3 +1,10 @@
+library(rgdal)
+
+
+#
+# prep main areas (Fdir) as of 2018
+#
+
 mainareaFdir2018 <- RstoxBase::DefineStratumPolygon(NULL, F, "ResourceFile", system.file("dataPrepScripts", "mainarea2018.txt", package="RstoxFDA"))
 
 if (is.na(sp::proj4string(mainareaFdir2018))){
@@ -5,6 +12,11 @@ if (is.na(sp::proj4string(mainareaFdir2018))){
 }
 
 usethis::use_data(mainareaFdir2018, overwrite = T)
+
+#
+# prep main areas (Fdir) before 2018
+#
+
 
 mainareaFdir2017 <- RstoxBase::DefineStratumPolygon(NULL, F, "ResourceFile", system.file("dataPrepScripts", "mainarea2017.txt", package="RstoxFDA"))
 if (is.na(sp::proj4string(mainareaFdir2017))){
@@ -21,3 +33,27 @@ slot(slot(mainareaFdir2017, "polygons")[[8]], "ID") <- "07"
 slot(slot(mainareaFdir2017, "polygons")[[9]], "ID") <- "08"
 slot(slot(mainareaFdir2017, "polygons")[[10]], "ID") <- "09"
 usethis::use_data(mainareaFdir2017, overwrite = T)
+
+#
+# prep locations (Fdir) as of 2018
+#
+
+
+locationsFdir2018 <- rgdal::readOGR("~/shapefiles/fdir/fdir_annotated/Lokasjoner_fom_2018/", stringsAsFactors = F)
+for (i in 1:nrow(locationsFdir2018)){
+  slot(slot(locationsFdir2018, "polygons")[[i]], "ID") <- locationsFdir2018$LOKREF[i]
+}
+locationsFdir2018$polygonName <- locationsFdir2018$LOKREF
+usethis::use_data(locationsFdir2018, overwrite = T)
+
+#
+# prep locations (Fdir) before 2018
+#
+
+locationsFdir2017 <- rgdal::readOGR("~/shapefiles/fdir/fdir_annotated/Lokasjoner_tom_2017/", stringsAsFactors = F)
+for (i in 1:nrow(locationsFdir2017)){
+  slot(slot(locationsFdir2017, "polygons")[[i]], "ID") <- locationsFdir2017$LOKREF[i]
+}
+locationsFdir2017$polygonName <- locationsFdir2017$LOKREF
+usethis::use_data(locationsFdir2017, overwrite = T)
+
