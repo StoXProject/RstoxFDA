6d893e8c4aa77cb06918211453b1382aaf263fad
edvinf <edvin.fuglebakk@gmail.com>
2020-11-08 10:59:16 +0100

    Fixed ID slots for mainarea pr 2018

--- inst/dataPrepScripts/prepSpatialResources.R
+++ inst/dataPrepScripts/prepSpatialResources.R
@@ -10,5 +10,14 @@ mainareaFdir2017 <- RstoxBase::DefineStratumPolygon(NULL, F, "ResourceFile", sys
 if (is.na(sp::proj4string(mainareaFdir2017))){
   sp::proj4string(mainareaFdir2017) <- "+proj=longlat +datum=WGS84"
 }
-
+slot(slot(mainareaFdir2017, "polygons")[[1]], "ID") <- "00"
+slot(slot(mainareaFdir2017, "polygons")[[2]], "ID") <- "01"
+slot(slot(mainareaFdir2017, "polygons")[[3]], "ID") <- "02"
+slot(slot(mainareaFdir2017, "polygons")[[4]], "ID") <- "03"
+slot(slot(mainareaFdir2017, "polygons")[[5]], "ID") <- "04"
+slot(slot(mainareaFdir2017, "polygons")[[6]], "ID") <- "05"
+slot(slot(mainareaFdir2017, "polygons")[[7]], "ID") <- "06"
+slot(slot(mainareaFdir2017, "polygons")[[8]], "ID") <- "07"
+slot(slot(mainareaFdir2017, "polygons")[[9]], "ID") <- "08"
+slot(slot(mainareaFdir2017, "polygons")[[10]], "ID") <- "09"
 usethis::use_data(mainareaFdir2017, overwrite = T)
