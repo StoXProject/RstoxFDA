c7dfc7bc28aaccc325ce21f32ff2fc19f3f40278
edvinf <edvin.fuglebakk@gmail.com>
2021-11-03 15:13:17 +0100

    Added some more tests for length group collapsing

---
+++ tests/testthat/test-lengthGroupCollapse.R
@@ -0,0 +1,32 @@
+StoxLandingFile <- system.file("testresources","StoxLandingData.rds", package="RstoxFDA")
+StoxLandingData <- readRDS(StoxLandingFile)
+
+StoxBioticFile <- system.file("testresources","StoxBioticData.rds", package="RstoxFDA")
+StoxBioticData <- readRDS(StoxBioticFile)
+StoxBioticData$Haul$Gear <- StoxLandingData$Landing$Gear[sample.int(20,45, replace=T)]
+
+fpath <- makeTempDirReca()
+
+prep <- PrepareRecaEstimate(StoxBioticData, StoxLandingData, FixedEffects = c(), RandomEffects = c("Gear"))
+paramOut <- ParameterizeRecaModels(prep, 10, 50, 1, fpath)
+
+context("RunRecaModels: Test collapse Length")
+resultsWlength <- RunRecaModels(paramOut, StoxLandingData, AggregationVariables = "Gear", CollapseLength = F)
+results <- RunRecaModels(paramOut, AggregationVariables = "Gear", StoxLandingData)
+
+removeTempDirReca(fpath)
+
+expect_gt(nrow(resultsWlength$CatchAtAge), nrow(results$CatchAtAge))
+
+
+reportLAAwLength <- ReportRecaLengthAtAge(resultsWlength)
+reportLAAwoLength <- ReportRecaLengthAtAge(results)
+expect_equal(reportLAAwLength$FdaReport$MeanIndividualLength, reportLAAwoLength$FdaReport$MeanIndividualLength)
+
+reportCAAwLength <- ReportRecaCatchAtAge(resultsWlength)
+reportCAAwoLength <- ReportRecaCatchAtAge(results)
+expect_equal(reportCAAwLength$FdaReport$CatchAtAge, reportCAAwoLength$FdaReport$CatchAtAge)
+
+reportWAAwLength <- ReportRecaLengthAtAge(resultsWlength)
+reportWAAwoLength <- ReportRecaLengthAtAge(results)
+expect_equal(reportWAAwLength$FdaReport$MeanIndividualWeight, reportWAAwoLength$FdaReport$MeanIndividualWeight)
