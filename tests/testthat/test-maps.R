e3e4cdcd328647bd7c22d7d25c0a18d4cedd106d
edvinf <edvin.fuglebakk@gmail.com>
2021-11-19 20:17:23 +0100

    Added some tests

---
+++ tests/testthat/test-maps.R
@@ -0,0 +1,34 @@
+data(ICESareas)
+plotArea(areaDef=ICESareas)
+
+data(mainareaFdir2018)
+data(NAFOareas)
+# plot mainarea and NAFO areas combined in a Lambert Conformal Conic projection.
+plotArea(title="Main area + NAFO",
+         areaDef=rbind(mainareaFdir2018[,c("StratumName")],
+                 NAFOareas[,c("StratumName")]),
+         projection="+proj=lcc +lat_1=43 +lat_2=62 +lat_0=30 
+         +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs")
+
+StoxBioticFile <- system.file("testresources","StoxBioticData.rds", package="RstoxFDA")
+StoxBioticData <- readRDS(StoxBioticFile)
+
+plotArea(title="Main area + NAFO",
+         data = StoxBioticData$Station,
+         latCol = "Latitude",
+         lonCol = "Longitude",
+         xlim = c(-14,14),
+         ylim = c(52,62),
+         areaDef=rbind(mainareaFdir2018[,c("StratumName")],
+                       NAFOareas[,c("StratumName")]))
+
+data(landings)
+data(ICESareas)
+plotBubbleMap(landings, "Area", "LiveWeightKG",
+      areaDef = ICESareas, areaNameCol = "Area_Full",
+      bubbleSize = 20, title="Landings on ICES areas", projection = "+proj=lcc +lat_1=43 +lat_2=62 +lat_0=30 
+         +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs")
+
+plotBubbleMap(landings, "Area", "LiveWeightKG",
+              areaDef = ICESareas, areaNameCol = "Area_Full",
+              bubbleSize = 20, title="Landings on ICES areas")
