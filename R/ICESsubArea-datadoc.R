ff797997ff8cc92cabfc06d8fbc3de8529fc74ea
Edvin Fuglebakk <Edvin.Fuglebakk@hi.no>
2024-02-03 21:04:19 +0100

    converted spatial ICEs area resources

--- R/ICESsubArea-datadoc.R
+++ R/ICESsubArea-datadoc.R
@@ -11,7 +11,7 @@
 #'
 #' @usage data(ICESsubArea)
 #'
-#' @format \code{\link[sp]{SpatialPolygonsDataFrame}} with area names identified in the column 'StratumName'. See \code{\link[RstoxBase]{StratumPolygon}}.
+#' @format \code{\link[sf]{sf}} with area names identified in the column 'StratumName'. See \code{\link[RstoxBase]{StratumPolygon}} (v.2).
 #'
 #' @concept area code polygons
 #' @keywords datasets
